18f0b89c755b28b96617b5706acd6b40c92bf97e
SuHwanYoon <ehtldy@naver.com>
2024-05-23 17:31:24 +0900

    add JavaScript practice directory

---
+++ jsPractice/app.js
@@ -0,0 +1,27 @@
+
+//javaScript List
+let hobbies = ['Sports','Coding','Reading'];
+// alert(hobbies[0])
+// alert(hobbies[1])
+// alert(hobbies[2])
+
+//JavaScript Object
+let job  = {
+    title:'개발자',
+    place:'남양주',
+    salary:3000
+};
+
+// alert(job.title);
+
+let yoonage;
+let exage = 20;
+
+function calMyAge() {
+    return exage + 13;
+}
+
+yoonage = calMyAge();
+alert("Yoon's age:"+ yoonage);
+
+
