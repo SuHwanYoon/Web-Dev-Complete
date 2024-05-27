8f28bd2a39c03c5cb0ec5d46acc1c2e6ceba9baf
SuHwanYoon <ehtldy@naver.com>
2024-05-27 17:50:03 +0900

    change let to const

--- jsPractice/advanced/demo.js
+++ jsPractice/advanced/demo.js
@@ -1,20 +1,20 @@
 //id로 input엘리먼트에 접근
-let inputTextElement = document.getElementById('input-text');
+const inputTextElement = document.getElementById('input-text');
 //id로 span 엘리먼트에 접근
-let remainingCharsElement = document.getElementById('remaining-chars');
+const remainingCharsElement = document.getElementById('remaining-chars');
 
 //input엘리먼트에 설정한 maxlength속성에 접근 - 60
-let maxAllowedChars = inputTextElement.maxLength;
+const maxAllowedChars = inputTextElement.maxLength;
 
 //이벤트가 실행될때마다 실행될 함수
 function updateRemainingCharaters(event) {
     //입력된 텍스트의 값 
-    let enteredText = event.target.value;
+    const enteredText = event.target.value;
     //입력된 텍스트의 값 길이
-    let enteredTextLength = enteredText.length;
+    const enteredTextLength = enteredText.length;
 
     //maxlength속성값 - 입력된 텍스트의 값 길이
-    let remainnigCharacters = maxAllowedChars - enteredTextLength;
+    const remainnigCharacters = maxAllowedChars - enteredTextLength;
 
     //span엘리먼트 값에 계산값을 넣어준다
     remainingCharsElement.textContent = remainnigCharacters;
